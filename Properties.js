2b7ae7c33ac61fd514536c1969a12b95a3e79cf4
jsbruner <jsbruner@umich.edu>
2017-11-21 16:36:26 -0500

    Almost change property values

--- Properties.js
+++ Properties.js
@@ -1,10 +1,26 @@
+function donePressed(ele_id) {
+  var ele = document.getElementById(ele_id)
+  ele.innerHTML = ""
+}
 
-function openEditDialog(obj, ele) {
-  var attributes = Object.keys(obj);
-  ele.innerHTML = "";
+function openEditDialog(obj, ele_id) {
+  var attributes = Object.keys(obj)
+  var ele = document.getElementById(ele_id)
+  ele.innerHTML = ""
 
   for (var i = 0; i < attributes.length; i++) {
-    ele.innerHTML += "<br>" + attributes[i] + " ";
-    ele.innerHTML += "<input value='" + obj[attributes[i]] + "'></input>"
+    ele.innerHTML += "<br>" + attributes[i] + " "
+    ele.innerHTML += "<input value='" + obj[attributes[i]] + "' id='property-" + i + "'></input>"
+
+
+  }
+
+  ele.innerHTML += "<br><button id='done-button' onclick='donePressed(\"" + ele_id + "\")'>Done</button>"
+
+  for (var i = 0; i < attributes.length; i++) {
+    document.getElementById("property-" + i).addEventListener("change", function(event) {
+      console.log(event)
+      obj[attributes[i]] = ""
+    });
   }
 }
