19061fd1d15ebbc9b4c6091df1f3e3e3a7e3bd82
Josiah Bruner <jsbruner@umich.edu>
2018-07-14 12:02:55 -0400

    Improve Properties UI to enable adding and proper editing of attributes

--- Properties.js
+++ Properties.js
@@ -8,19 +8,32 @@ function openEditDialog(obj, ele_id) {
   var ele = document.getElementById(ele_id)
   ele.innerHTML = ""
 
+  // Display existing attributes
   for (var i = 0; i < attributes.length; i++) {
     ele.innerHTML += "<br>" + attributes[i] + " "
     ele.innerHTML += "<input value='" + obj[attributes[i]] + "' id='property-" + i + "'></input>"
-
-
   }
 
-  ele.innerHTML += "<br><button id='done-button' onclick='donePressed(\"" + ele_id + "\")'>Done</button>"
+  // UI for adding attributes
+  ele.innerHTML += "<br><br>Add attribute:"
+  ele.innerHTML += "<input id='new-attr-id-field' placeholder='New ID'></input> <input id='new-attr-val-field' placeholder='New Value'></input>"
+  ele.innerHTML += " <button id='add-button'>Add Attribute</button>"
 
+  ele.innerHTML += "<br><br><button id='done-button' onclick='donePressed(\"" + ele_id + "\")'>Done</button>"
+
+  // Event listeners for editing attributes
   for (var i = 0; i < attributes.length; i++) {
     document.getElementById("property-" + i).addEventListener("change", function(event) {
       console.log(event)
-      obj[attributes[i]] = ""
+      obj[attributes[event.target.id.split("-").pop()]] = document.getElementById(event.target.id).value
     });
   }
+
+  document.getElementById("add-button").addEventListener("click", function(event) {
+    console.log("Add clicked")
+    // Save added attribute if one exists
+    if (document.getElementById("new-attr-id-field").value !== "") {
+      obj[document.getElementById("new-attr-id-field").value] = document.getElementById("new-attr-val-field").value
+    }
+  });
 }
