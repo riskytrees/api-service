88d464515ded441bc787ff5e553d85a5940b1e73
Josiah Bruner <jsbruner@umich.edu>
2018-07-14 12:16:20 -0400

    Display name in properties dialog

--- Properties.js
+++ Properties.js
@@ -4,18 +4,22 @@ function donePressed(ele_id) {
 }
 
 function openEditDialog(obj, ele_id) {
-  var attributes = Object.keys(obj)
+  var attributes = Object.keys(obj.attributes)
+  var label = obj.label
   var ele = document.getElementById(ele_id)
   ele.innerHTML = ""
 
+  // Display name
+  ele.innerHTML += "<br>Name: " + label + " "
+
   // Display existing attributes
   for (var i = 0; i < attributes.length; i++) {
-    ele.innerHTML += "<br>" + attributes[i] + " "
-    ele.innerHTML += "<input value='" + obj[attributes[i]] + "' id='property-" + i + "'></input>"
+    ele.innerHTML += "<br><br>" + attributes[i] + " "
+    ele.innerHTML += "<input value='" + obj.attributes[attributes[i]] + "' id='property-" + i + "'></input>"
   }
 
   // UI for adding attributes
-  ele.innerHTML += "<br><br>Add attribute:"
+  ele.innerHTML += "<br><br>Add attribute: "
   ele.innerHTML += "<input id='new-attr-id-field' placeholder='New ID'></input> <input id='new-attr-val-field' placeholder='New Value'></input>"
   ele.innerHTML += " <button id='add-button'>Add Attribute</button>"
 
@@ -25,7 +29,7 @@ function openEditDialog(obj, ele_id) {
   for (var i = 0; i < attributes.length; i++) {
     document.getElementById("property-" + i).addEventListener("change", function(event) {
       console.log(event)
-      obj[attributes[event.target.id.split("-").pop()]] = document.getElementById(event.target.id).value
+      obj.attributes[attributes[event.target.id.split("-").pop()]] = document.getElementById(event.target.id).value
     });
   }
 
@@ -33,7 +37,7 @@ function openEditDialog(obj, ele_id) {
     console.log("Add clicked")
     // Save added attribute if one exists
     if (document.getElementById("new-attr-id-field").value !== "") {
-      obj[document.getElementById("new-attr-id-field").value] = document.getElementById("new-attr-val-field").value
+      obj.attributes[document.getElementById("new-attr-id-field").value] = document.getElementById("new-attr-val-field").value
     }
   });
 }
