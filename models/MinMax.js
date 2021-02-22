6601bf8cf4010acb2260c8e0bcbfbf879dfe727a
Josiah Bruner <jsbruner@umich.edu>
2021-02-22 16:46:11 -0500

    Updates to MinMax

--- models/MinMax.js
+++ models/MinMax.js
@@ -3,70 +3,68 @@
 /* global Node */
 
 class MinMaxNode extends Node {
-   constructor (id, label, attributeObj) {
-      super(id, label, attributeObj)
-   }
+  constructor (id, label, attributeObj) {
+    super(id, label, attributeObj)
+  }
 
-   static getUUID () {
-      return '7df52155-0330-435b-b022-55586ec188a1'
-   }
+  static getUUID () {
+    return '7df52155-0330-435b-b022-55586ec188a1'
+  }
 
-   modelLabelDisplay (edgesStore, nodesStore) {
-      let effortStr = '?'
+  modelLabelDisplay (edgesStore, nodesStore) {
+    let effortStr = '?'
 
-      if (this.attributes && this.attributes.effort && this.getChildren(edgesStore, nodesStore).length === 0) {
-         const effort = parseInt(this.attributes.effort, 10)
-         effortStr = effort.toString()
-      } else if (this.attributes && this.attributes.operator && this.getChildren(edgesStore, nodesStore).length > 0) {
-         let effort = null;
+    if (this.attributes && this.attributes.effort && this.getChildren(edgesStore, nodesStore).length === 0) {
+      const effort = parseInt(this.attributes.effort, 10)
+      effortStr = effort.toString()
+    } else if (this.attributes && this.attributes.operator && this.getChildren(edgesStore, nodesStore).length > 0) {
+      let effort = null
 
-         if (this.attributes.operator === 'min' || this.attributes.operator === 'or') {
-            // Min - Get easiest thing
-            effort = this.getMinEffort(this.getChildren(edgesStore, nodesStore))
-         } else if (this.attributes.operator === 'max' || this.attributes.operator === 'and') {
-            // Max - Get hardest thing
-            effort = this.getMaxEffort(this.getChildren(edgesStore, nodesStore))
-         }
-
-         if (effort) {
-            effortStr = effort.toString()
-         }
+      if (this.attributes.operator === 'min' || this.attributes.operator === 'or') {
+        // Min - Get easiest thing
+        effort = this.getMinEffort(this.getChildren(edgesStore, nodesStore))
+      } else if (this.attributes.operator === 'max' || this.attributes.operator === 'and') {
+        // Max - Get hardest thing
+        effort = this.getMaxEffort(this.getChildren(edgesStore, nodesStore))
       }
 
-      return this.label + '\nEffort: ' + effortStr
-   }
-
-   getMinEffort (nodes) {
-      let lowestEffort = null;
-
-      for (const child of nodes) {
-         if (child.attributes.effort) {
-            const childEffort = parseInt(child.attributes.effort, 10)
-
-            if (!lowestEffort || childEffort < lowestEffort) {
-               lowestEffort = childEffort
-            }
-         }
-
+      if (effort) {
+        effortStr = effort.toString()
       }
+    }
 
-      return lowestEffort
-   }
+    return this.label + '\nEffort: ' + effortStr
+  }
 
-   getMaxEffort (nodes) {
-      let highestEffort = null;
+  getMinEffort (nodes) {
+    let lowestEffort = null
 
-      for (const child of nodes) {
-         if (child.attributes.effort) {
-            const childEffort = parseInt(child.attributes.effort, 10)
+    for (const child of nodes) {
+      if (child.attributes.effort) {
+        const childEffort = parseInt(child.attributes.effort, 10)
 
-            if (!highestEffort || childEffort > highestEffort) {
-               highestEffort = childEffort
-            }
-         }
-
+        if (!lowestEffort || childEffort < lowestEffort) {
+          lowestEffort = childEffort
+        }
       }
+    }
 
-      return highestEffort
-   }
+    return lowestEffort
+  }
+
+  getMaxEffort (nodes) {
+    let highestEffort = null
+
+    for (const child of nodes) {
+      if (child.attributes.effort) {
+        const childEffort = parseInt(child.attributes.effort, 10)
+
+        if (!highestEffort || childEffort > highestEffort) {
+          highestEffort = childEffort
+        }
+      }
+    }
+
+    return highestEffort
+  }
 }
