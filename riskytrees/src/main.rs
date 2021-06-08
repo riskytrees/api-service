36fb7074fb683cec9267ae70229796b7b698354f
Josiah Bruner <jsbruner@umich.edu>
2021-06-08 18:39:41 -0400

    Auth login pseudo implementation

--- riskytrees/src/main.rs
+++ riskytrees/src/main.rs
@@ -6,7 +6,6 @@ use mongodb::{
     bson::{doc},
 };
 use rocket_contrib::json::Json;
-
 mod database;
 mod constants;
 mod models;
@@ -24,16 +23,34 @@ fn index() -> &'static str {
 }
 
 #[post("/auth/login", data = "<body>")]
-fn auth_login(body: Json<models::ApiRegisterUser>) -> &'static str {
+fn auth_login(body: Json<models::ApiRegisterUser>) -> Json<models::ApiAuthLoginResponse> {
     let db_client = database::get_instance();
     match db_client {
         Ok(client) => {
             if database::get_user(client.to_owned(), body.email.to_owned()).is_none() {
                 database::new_user(client, body.email.to_owned());
+
+                Json(models::ApiAuthLoginResponse {
+                    ok: true,
+                    message: "User created and logged in succesfully".to_owned(),
+                    result: Some(models::AuthLoginResponseResult {
+                        sessionToken: "testtoken".to_owned()
+                    })
+                })
+            } else {
+                Json(models::ApiAuthLoginResponse {
+                    ok: true,
+                    message: "User logged in succesfully".to_owned(),
+                    result: None
+                })
+
             }
-            "Hello, world!"
         },
-        Err(e) => "Failed to create user"
+        Err(e) =>  Json(models::ApiAuthLoginResponse {
+            ok: false,
+            message: "Could not connect to DB".to_owned(),
+            result: None
+        })
     }
 }
 
--- riskytrees/src/models/mod.rs
+++ riskytrees/src/models/mod.rs
@@ -1,10 +1,23 @@
-use serde::Deserialize;
+use serde::{Serialize, Serializer, Deserialize};
 
+#[derive(Serialize, Deserialize, Debug)]
 pub struct User {
     pub email: String
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct AuthLoginResponseResult {
+    pub sessionToken: String
 }
 
 #[derive(Deserialize)]
 pub struct ApiRegisterUser {
     pub email: String
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ApiAuthLoginResponse {
+    pub ok: bool,
+    pub message: String,
+    pub result: Option<AuthLoginResponseResult>
 }
