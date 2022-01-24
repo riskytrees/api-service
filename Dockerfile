15b07092161bf4d9c9ea0ef46d1a5462f2d06175
Josiah Bruner (:JosiahOne) <jsbruner@umich.edu>
2022-01-23 23:18:26 -0500

    Add support for AWS deployments (#22)

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM rust:1-buster
+COPY riskytrees /app/riskytrees
+COPY cas/rds-combined-ca-bundle.pem /app/riskytrees/rds-combined-ca-bundle.pem
+
+WORKDIR /app/riskytrees
+
+RUN cargo build
+ENTRYPOINT ["cargo", "run"]
--- riskytrees/src/constants/mod.rs
+++ riskytrees/src/constants/mod.rs
@@ -1,2 +1,10 @@
+use std::env;
+
 pub const DATABASE_NAME: &str = "riskytrees";
-pub const DATABASE_HOST: &str = "mongodb://localhost:27017";
+
+pub fn get_database_host() -> String {
+    match env::var("DATABASE_HOST") {
+        Ok(val) => val,
+        Err(err) => "mongodb://localhost:27017".to_owned()
+    }
+}
