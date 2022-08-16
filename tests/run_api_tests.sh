c825bc30ea5528ff759c232359fd8b7d713a67d6
Josiah Bruner <josiah@josiahbruner.com>
2022-08-15 22:54:41 -0400

    Do not auto cleanup

--- tests/run_api_tests.sh
+++ tests/run_api_tests.sh
@@ -1,10 +1,8 @@
 cd ..
+docker-compose down
 docker-compose up -d
 
 cd tests/
 python3 -m venv env;
 source env/bin/activate && python3 -m pip install -r requirements.txt;
 source env/bin/activate && pytest
-
-cd ..
-docker-compose down
