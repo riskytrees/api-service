aaa402676bd2381ca3b6c137619030eaf7bd7c27
Josiah Bruner <josiah@josiahbruner.com>
2024-07-27 17:53:55 -0400

    frfr

--- riskytrees/src/tests/mod.rs
+++ riskytrees/src/tests/mod.rs
@@ -19,7 +19,7 @@ fn test_de_json_condition() {
 fn test_expression_evaluation() {
     let config = models::ApiProjectConfigResponseResult {
         id: "test".to_owned(),
-        name: "Hello",
+        name: Some("Hello".to_owned()),
         attributes: serde_json::json!({
             "hello": "world",
             "other": false
