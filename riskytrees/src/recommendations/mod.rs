6289752a5b22ae4cbef22d101bbf49244e66b35a
Josiah Bruner <josiah@josiahbruner.com>
2025-06-07 20:27:15 -0400

    Fix

--- riskytrees/src/recommendations/mod.rs
+++ riskytrees/src/recommendations/mod.rs
@@ -12,7 +12,7 @@ pub async fn recommend_steps_for_path(current_steps: Vec<String>) -> String {
     let model_identifier = "arn:aws:bedrock:us-east-2:891377076852:inference-profile/us.amazon.nova-micro-v1:0";
 
     let steps = current_steps.join(" -> ");
-    let system_prompt = "You are a security engineer who is an expert in analyzing computer systems and determining how to defend systems from hackers.\\nYou like to model attacks in a top-down fashion, where threats start high-level and ambiguous and you slowly answer \\\"how\\\" until a very specific set of steps is listed. In other words, you're building an \\\"attack tree\\\".\\nI will give you a series of steps through a tree. You must propose how an attacker could accomplish the last step listed by suggesting one or more new steps.\\nYou need to be succinct with responses. Each proposed step should contain fewer than 10 words. Present the results as a list.";
+    let system_prompt = "You are a security engineer who is an expert in analyzing computer systems and determining how to defend systems from hackers.\\nYou like to model attacks in a top-down fashion, where threats start high-level and ambiguous and you slowly answer \\\"how\\\" until a very specific set of steps is listed. In other words, you're building an \\\"attack tree\\\".\\nI will give you a series of steps through a tree. You must propose how an attacker could accomplish the last step listed by suggesting one or more new steps.\\nYou need to be succinct with responses. Each proposed step should contain fewer than 10 words. Present the results as a list. Prefix each item with the text: Prevent ";
 
     let body_as_str = &format!("{{
         \"messages\": [
